021778d4ee8989c5f8f661a79c1532c640583a05
Robert Stolz <robert.stolz87@gmail.com>
2025-06-12 14:12:57 +0200

    Country controller

--- backend/src/main/java/ropold/backend/controller/CountryController.java
+++ backend/src/main/java/ropold/backend/controller/CountryController.java
@@ -1,11 +1,158 @@
 package ropold.backend.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ropold.backend.model.CountryModel;
+import ropold.backend.service.CloudinaryService;
+import ropold.backend.service.CountryService;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/world-quartet-hub")
 @RequiredArgsConstructor
 public class CountryController {
+
+    private final CountryService countryService;
+    private final CloudinaryService cloudinaryService;
+
+    @GetMapping
+    public List<CountryModel> getAllCountries() {
+        return countryService.getAllCountries();
+    }
+
+    @GetMapping("/{id}")
+    public CountryModel getCountryById(@PathVariable String id) {
+        CountryModel country = countryService.getCountryById(id);
+        if (country == null) {
+            throw new CountryNotFoundException("No Country found with id: " + id);
+        }
+        return country;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping()
+    public CountryModel addCountry(
+            @RequestPart("countryModel") CountryModel countryModel,
+            @RequestPart(value = "image", required = false) MultipartFile image,
+            @AuthenticationPrincipal OAuth2User authentication) throws IOException {
+
+        String authenticatedUserId = authentication.getName();
+
+        String imageUrl = null;
+        if (image != null && !image.isEmpty()) {
+            imageUrl = cloudinaryService.uploadImage(image);
+        }
+
+        return countryService.addCountry(
+                new CountryModel(
+                        null,
+                        countryModel.countryName(),
+                        countryModel.capitalCity(),
+                        countryModel.populationInMillions(),
+                        countryModel.populationDensityPerKm2(),
+                        countryModel.capitalCityPopulation(),
+                        countryModel.gdpPerCapitaInUSD(),
+                        countryModel.forestAreaPercentage(),
+                        countryModel.totalAreaInKm2(),
+                        countryModel.roadNetworkLengthInKm(),
+                        countryModel.averageAnnualTemperatureInC(),
+                        countryModel.annualPrecipitationInMm(),
+                        authenticatedUserId,
+                        imageUrl
+                )
+        );
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/no-login")
+    public CountryModel addCountryWithNoLogin(
+            @RequestBody CountryModel countryModel){
+
+        return countryService.addCountry(
+                        new CountryModel(
+                                null,
+                                countryModel.countryName(),
+                                countryModel.capitalCity(),
+                                countryModel.populationInMillions(),
+                                countryModel.populationDensityPerKm2(),
+                                countryModel.capitalCityPopulation(),
+                                countryModel.gdpPerCapitaInUSD(),
+                                countryModel.forestAreaPercentage(),
+                                countryModel.totalAreaInKm2(),
+                                countryModel.roadNetworkLengthInKm(),
+                                countryModel.averageAnnualTemperatureInC(),
+                                countryModel.annualPrecipitationInMm(),
+                                countryModel.githubId(),
+                                null
+                        )
+                );
+    }
+
+    @PutMapping("/{id}")
+    public CountryModel updateCountry(
+            @PathVariable String id,
+            @RequestPart("countryModel") CountryModel countryModel,
+            @RequestPart(value = "image", required = false) MultipartFile image,
+            @AuthenticationPrincipal OAuth2User authentication) throws IOException {
+
+        String authenticatedUserId = authentication.getName();
+        CountryModel existingCountry = countryService.getQuestionById(id);
+
+        if (!authenticatedUserId.equals(existingCountry.githubId())) {
+            throw new AccessDeniedException("You do not have permission to update this country.");
+        }
+
+        String newImageUrl;
+
+        if (image != null && !image.isEmpty()) {
+            newImageUrl = cloudinaryService.uploadImage(image);
+        } else if (countryModel.imageUrl() == null || countryModel.imageUrl().isBlank()) {
+            newImageUrl = null;
+        } else {
+            newImageUrl = existingCountry.imageUrl();
+        }
+
+        CountryModel updatedCountry = new CountryModel(
+                id,
+                countryModel.countryName(),
+                countryModel.capitalCity(),
+                countryModel.populationInMillions(),
+                countryModel.populationDensityPerKm2(),
+                countryModel.capitalCityPopulation(),
+                countryModel.gdpPerCapitaInUSD(),
+                countryModel.forestAreaPercentage(),
+                countryModel.totalAreaInKm2(),
+                countryModel.roadNetworkLengthInKm(),
+                countryModel.averageAnnualTemperatureInC(),
+                countryModel.annualPrecipitationInMm(),
+                authenticatedUserId,
+                newImageUrl
+        );
+
+        return countryService.updateCountry(updatedCountry);
+    }
+
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCountry(@PathVariable String id, @AuthenticationPrincipal OAuth2User authentication) {
+        String authenticatedUserId = authentication.getName();
+        CountryModel countryModel = countryService.getCountryById(id);
+
+        if (!authenticatedUserId.equals(countryModel.githubId())) {
+            throw new AccessDeniedException("You do not have permission to delete this country.");
+        }
+        countryService.deleteCountry(id);
+    }
+
+
+
 }
+
