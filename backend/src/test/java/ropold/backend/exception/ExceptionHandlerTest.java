95548dd301f96facfbecdec701bbe5a614000f07
Robert Stolz <robert.stolz87@gmail.com>
2025-06-16 09:54:50 +0200

    ehandler tests complete

--- backend/src/test/java/ropold/backend/exception/ExceptionHandlerTest.java
+++ backend/src/test/java/ropold/backend/exception/ExceptionHandlerTest.java
@@ -1,16 +1,25 @@
 package ropold.backend.exception;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.Uploader;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import ropold.backend.repository.CountryRepository;
 import ropold.backend.service.CountryService;
 
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -37,5 +46,24 @@ class ExceptionHandlerTest {
                 .andExpect(jsonPath("$.message").value("No Country found with id: non-existing-id"));
     }
 
+    @Test
+    void whenRuntimeException_thenReturnsInternalServerError() throws Exception {
+        when(countryService.getCountryById(any())).thenThrow(new RuntimeException("Unexpected error"));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/world-quartet-hub/{id}", "any-id"))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.message").value("Unexpected error"));
+
+    }
+
+    @Test
+    @WithMockUser(username = "user")
+    void whenAccessDeniedException_thenReturnsForbidden() throws Exception {
+        when(countryService.getCountryById(any())).thenThrow(new AccessDeniedException("Access denied"));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/world-quartet-hub/{id}", "any-id"))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.message").value("Access denied"));
+    }
 
 }
