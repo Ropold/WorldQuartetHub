dc7c43b91a528f20460f2a88b7067e49b0b82745
Robert Stolz <robert.stolz87@gmail.com>
2025-06-16 09:24:58 +0200

    CountryServiceTests complete

--- backend/src/test/java/ropold/backend/service/CountryServiceTest.java
+++ backend/src/test/java/ropold/backend/service/CountryServiceTest.java
@@ -75,22 +75,79 @@ class CountryServiceTest {
 
     @Test
     void testAddCountry(){
+        CountryModel countryModel3 = new CountryModel(
+                null,
+                "Italien",
+                "Rom",
+                60.4,
+                200,
+                2873000,
+                35000,
+                31,
+                301340,
+                1000000,
+                15.5,
+                900,
+                "user",
+                "http://example.com/country3.jpg"
+        );
+
+        when(idService.generateRandomId()).thenReturn("3");
+        when(countryRepository.save(any(CountryModel.class))).thenReturn(countryModel3);
+
+        CountryModel expected = countryService.addCountry(countryModel3);
+        assertEquals(countryModel3,expected);
+        verify(idService, times(1)).generateRandomId();
+        verify(countryRepository).save(any(CountryModel.class));
 
     }
 
     @Test
     void testUpdateCountry(){
+        CountryModel updatedCountry = new CountryModel(
+                "1",
+                "Deutschland",
+                "Berlin",
+                83.2,
+                233,
+                3769000,
+                51600,
+                33,
+                357582,
+                644480,
+                8.5,
+                700,
+                "user",
+                "http://example.com/updated_country1.jpg"
+        );
 
+        when(countryRepository.findById("1")).thenReturn(java.util.Optional.of(countryModels.get(0)));
+        when(countryRepository.save(updatedCountry)).thenReturn(updatedCountry);
+
+        CountryModel result = countryService.updateCountry(updatedCountry);
+        assertEquals(updatedCountry, result);
+        verify(countryRepository).save(updatedCountry);
     }
 
     @Test
     void testDeleteCountry(){
-
+        CountryModel countryModel = countryModels.getFirst();
+        when(countryRepository.findById(countryModel.id())).thenReturn(java.util.Optional.of(countryModel));
+        countryService.deleteCountry("1");
+        verify(countryRepository, times(1)).deleteById("1");
+        verify(cloudinaryService, times(1)).deleteImage(countryModel.imageUrl());
     }
 
     @Test
     void testGetCountriesForGithubUser(){
-
+        String githubId = "user";
+        List<CountryModel> expectedCountries = countryModels.stream()
+                .filter(country -> country.githubId().equals(githubId))
+                .toList();
+        when(countryRepository.findAll()).thenReturn(countryModels);
+        List<CountryModel> result = countryService.getCountriesForGithubUser(githubId);
+        assertEquals(expectedCountries, result);
+        verify(countryRepository, times(1)).findAll();
     }
 
     @Test
@@ -100,6 +157,5 @@ class CountryServiceTest {
         List<CountryModel> result = countryService.getCountriesByIds(ids);
         assertEquals(countryModels, result);
     }
-
 
 }
