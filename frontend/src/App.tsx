2896c2b1dde8356944a694ee6e35e575c5287b60
Robert Stolz <robert.stolz87@gmail.com>
2025-06-17 17:34:36 +0200

    protected Routes - first setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,39 @@
 import './App.css'
+import {useEffect, useState} from "react";
+import axios from "axios";
+import ProtectedRoute from "./components/ProtectedRoute.tsx";
+import {Route, Routes} from "react-router-dom";
+import NotFound from "./components/NotFound.tsx";
+import Welcome from "./components/Welcome.tsx";
+import Profile from "./components/Profile.tsx";
 
 export default function App() {
+    const [user, setUser] = useState<string>("anonymousUser");
+
+    function getUser() {
+        axios.get("/api/users/me")
+            .then((response) => {
+                setUser(response.data.toString());
+            })
+            .catch((error) => {
+                console.error(error);
+                setUser("anonymousUser");
+            });
+    }
+
+    useEffect(() => {
+        getUser();
+    }, []);
 
   return (
     <>
-      <h1>World Quartet Hub</h1>
+        <Routes>
+            <Route path="*" element={<NotFound />} />
+            <Route path="/" element={<Welcome/>}/>
+            <Route element={<ProtectedRoute user={user}/>}>
+                <Route path="/profile/*" element={<Profile />} />
+            </Route>
+        </Routes>
     </>
   )
 }
