0da047a1b3334dc79347d7f9f603573cea96e2bd
Robert Stolz <robert.stolz87@gmail.com>
2025-06-17 18:28:49 +0200

    navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,9 +6,13 @@ import {Route, Routes} from "react-router-dom";
 import NotFound from "./components/NotFound.tsx";
 import Welcome from "./components/Welcome.tsx";
 import Profile from "./components/Profile.tsx";
+import type {UserDetails} from "./components/model/UserDetailsModel.ts";
+import Footer from "./components/Footer.tsx";
+import Navbar from "./components/Navbar.tsx";
 
 export default function App() {
     const [user, setUser] = useState<string>("anonymousUser");
+    const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
 
     function getUser() {
         axios.get("/api/users/me")
@@ -21,12 +25,30 @@ export default function App() {
             });
     }
 
+    function getUserDetails() {
+        axios.get("/api/users/me/details")
+            .then((response) => {
+                setUserDetails(response.data as UserDetails);
+            })
+            .catch((error) => {
+                console.error(error);
+                setUserDetails(null);
+            });
+    }
+
     useEffect(() => {
         getUser();
     }, []);
 
+    useEffect(() => {
+        if(user !== "anonymousUser"){
+            getUserDetails();
+        }
+    }, [user]);
+
   return (
     <>
+        <Navbar getUser={getUser} getUserDetails={getUserDetails} user={user}/>
         <Routes>
             <Route path="*" element={<NotFound />} />
             <Route path="/" element={<Welcome/>}/>
@@ -34,6 +56,7 @@ export default function App() {
                 <Route path="/profile/*" element={<Profile />} />
             </Route>
         </Routes>
+        <Footer />
     </>
   )
 }
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,81 @@
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
+import "./styles/Navbar.css";
+import headerLogo from "../assets/world-quartet-original.jpg"
+
+type NavbarProps = {
+    getUser: () => void;
+    getUserDetails: () => void;
+    user: string;
+}
+
+export default function Navbar(props: Readonly<NavbarProps>) {
+
+    const navigate = useNavigate();
+
+    function loginWithGithub() {
+        const host = window.location.host === "localhost:5173" ? "http://localhost:8080" : window.location.origin;
+        window.open(host + "/oauth2/authorization/github", "_self");
+    }
+
+    function logoutFromGithub() {
+        axios
+            .post("/api/users/logout")
+            .then(() => {
+                props.getUser();
+                props.getUserDetails();
+                navigate("/");
+            })
+            .catch((error) => {
+                console.error("Logout failed:", error);
+            });
+    }
+
+
+    return (
+        <nav className="navbar">
+            <button className="button-group-button" onClick={() => navigate("/")}>Home</button>
+            <div
+                className="clickable-header"
+                id="clickable-header-play"
+                onClick={() => {
+                    navigate("/play");
+                }}
+            >
+                <h2 className="header-title">Play</h2>
+                <img src={headerLogo} alt="World Quartet Hub Logo" className="logo-image" />
+            </div>
+
+            <div
+                className="clickable-header"
+                onClick={() => {
+                    navigate("/list-of-all-countries");
+                }}
+            >
+                <h2 className="header-title">All Countries</h2>
+                <img src={headerLogo} alt="All Countries Logo" className="logo-image" />
+            </div>
+
+            {/*<div*/}
+            {/*    className="clickable-header"*/}
+            {/*    id="button-high-score"*/}
+            {/*    onClick={() => {*/}
+            {/*        navigate("/high-score");*/}
+            {/*    }}*/}
+            {/*>*/}
+            {/*    <h2 className="header-title">High Score</h2>*/}
+            {/*    <img src={highScoreLogo} alt="High Score Logo" className="logo-image" />*/}
+            {/*</div>*/}
+
+
+            {props.user !== "anonymousUser" ? (
+                <>
+                    <button id="button-profile" onClick={() => navigate("/profile")}>Profile</button>
+                    <button className="button-group-button" onClick={logoutFromGithub}>Logout</button>
+                </>
+            ) : (
+                <button className="button-group-button" onClick={loginWithGithub}>Login with GitHub</button>
+            )}
+        </nav>
+    );
+}
