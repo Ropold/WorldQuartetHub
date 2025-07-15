e8fffc9ed0647e5ba9163ff8322c97d96dffdeaf
Robert Stolz <robert.stolz87@gmail.com>
2025-07-15 16:52:16 +0200

    region images

---
+++ frontend/src/components/utils/RegionImages.ts
@@ -0,0 +1,284 @@
+import afRegionPic from "../../assets/country-region/Afghanistan.gif"
+import alRegionPic from "../../assets/country-region/Albania.gif"
+import dzRegionPic from "../../assets/country-region/Algeria.gif"
+import aoRegionPic from "../../assets/country-region/Angola.gif"
+import arRegionPic from "../../assets/country-region/Argentina.gif"
+import auRegionPic from "../../assets/country-region/Australia.gif"
+import atRegionPic from "../../assets/country-region/Austria.gif"
+import azRegionPic from "../../assets/country-region/Azerbaijan.gif"
+import bdRegionPic from "../../assets/country-region/Bangladesh.gif"
+import byRegionPic from "../../assets/country-region/Belarus.gif"
+import beRegionPic from "../../assets/country-region/Belgium.gif"
+import bjRegionPic from "../../assets/country-region/Benin.gif"
+import boRegionPic from "../../assets/country-region/Bolivia.gif"
+import bgRegionPic from "../../assets/country-region/Bulgaria.gif"
+import bfRegionPic from "../../assets/country-region/Burkina Faso.gif"
+import biRegionPic from "../../assets/country-region/Burundi.gif"
+import khRegionPic from "../../assets/country-region/Cambodia.gif"
+import cmRegionPic from "../../assets/country-region/Cameroon.gif"
+import caRegionPic from "../../assets/country-region/Canada.gif"
+import cfRegionPic from "../../assets/country-region/Central African Republic.gif"
+import tdRegionPic from "../../assets/country-region/Chad.gif"
+import clRegionPic from "../../assets/country-region/Chile.gif"
+import cnRegionPic from "../../assets/country-region/China.gif"
+import coRegionPic from "../../assets/country-region/Colombia.gif"
+import cgRegionPic from "../../assets/country-region/Congo.gif"
+import crRegionPic from "../../assets/country-region/Costa Rica.gif"
+import hrRegionPic from "../../assets/country-region/Croatia.gif"
+import cuRegionPic from "../../assets/country-region/Cuba.gif"
+import czRegionPic from "../../assets/country-region/Czechia.gif"
+import dkRegionPic from "../../assets/country-region/Denmark.gif"
+import doRegionPic from "../../assets/country-region/Dominican Republic.gif"
+import ecRegionPic from "../../assets/country-region/Ecuador.gif"
+import egRegionPic from "../../assets/country-region/Egypt.gif"
+import svRegionPic from "../../assets/country-region/El Salvador.gif"
+import erRegionPic from "../../assets/country-region/Eritrea.gif"
+import eeRegionPic from "../../assets/country-region/Estonia.gif"
+import etRegionPic from "../../assets/country-region/Ethiopia.gif"
+import fjRegionPic from "../../assets/country-region/Fiji.gif"
+import fiRegionPic from "../../assets/country-region/Finland.gif"
+import frRegionPic from "../../assets/country-region/France.gif"
+import gaRegionPic from "../../assets/country-region/Gabon.gif"
+import gmRegionPic from "../../assets/country-region/Gambia.gif"
+import geRegionPic from "../../assets/country-region/Georgia.gif"
+import deRegionPic from "../../assets/country-region/Germany.gif"
+import ghRegionPic from "../../assets/country-region/Ghana.gif"
+import grRegionPic from "../../assets/country-region/Greece.gif"
+import gtRegionPic from "../../assets/country-region/Guatemala.gif"
+import htRegionPic from "../../assets/country-region/Haiti.gif"
+import hnRegionPic from "../../assets/country-region/Honduras.gif"
+import huRegionPic from "../../assets/country-region/Hungary.gif"
+import isRegionPic from "../../assets/country-region/Iceland.gif"
+import inRegionPic from "../../assets/country-region/India.gif"
+import idRegionPic from "../../assets/country-region/Indonesia.gif"
+import irRegionPic from "../../assets/country-region/Iran.gif"
+import iqRegionPic from "../../assets/country-region/Iraq.gif"
+import ieRegionPic from "../../assets/country-region/Ireland.gif"
+import ilRegionPic from "../../assets/country-region/Israel.gif"
+import itRegionPic from "../../assets/country-region/Italy.gif"
+import jpRegionPic from "../../assets/country-region/Japan.gif"
+import joRegionPic from "../../assets/country-region/Jordan.gif"
+import kzRegionPic from "../../assets/country-region/Kazakhstan.gif"
+import keRegionPic from "../../assets/country-region/Kenya.gif"
+import kwRegionPic from "../../assets/country-region/Kuwait.gif"
+import kgRegionPic from "../../assets/country-region/Kyrgyzstan.gif"
+import laRegionPic from "../../assets/country-region/Laos.gif"
+import lvRegionPic from "../../assets/country-region/Latvia.gif"
+import lbRegionPic from "../../assets/country-region/Lebanon.gif"
+import lrRegionPic from "../../assets/country-region/Liberia.gif"
+import lyRegionPic from "../../assets/country-region/Libya.gif"
+import ltRegionPic from "../../assets/country-region/Lithuania.gif"
+import luRegionPic from "../../assets/country-region/Luxembourg.gif"
+import mgRegionPic from "../../assets/country-region/Madagascar.gif"
+import myRegionPic from "../../assets/country-region/Malaysia.gif"
+import mvRegionPic from "../../assets/country-region/Maldives.gif"
+import mtRegionPic from "../../assets/country-region/Malta.gif"
+import mxRegionPic from "../../assets/country-region/Mexico.gif"
+import mdRegionPic from "../../assets/country-region/Moldova.gif"
+import mnRegionPic from "../../assets/country-region/Mongolia.gif"
+import meRegionPic from "../../assets/country-region/Montenegro.gif"
+import maRegionPic from "../../assets/country-region/Morocco.gif"
+import mzRegionPic from "../../assets/country-region/Mozambique.gif"
+import mmRegionPic from "../../assets/country-region/Myanmar.gif"
+import naRegionPic from "../../assets/country-region/Namibia.gif"
+import npRegionPic from "../../assets/country-region/Nepal.gif"
+import nlRegionPic from "../../assets/country-region/Netherlands.gif"
+import nzRegionPic from "../../assets/country-region/New Zealand.gif"
+import niRegionPic from "../../assets/country-region/Nicaragua.gif"
+import neRegionPic from "../../assets/country-region/Niger.gif"
+import ngRegionPic from "../../assets/country-region/Nigeria.gif"
+import kpRegionPic from "../../assets/country-region/North Korea.gif"
+import noRegionPic from "../../assets/country-region/Norway.gif"
+import omRegionPic from "../../assets/country-region/Oman.gif"
+import pkRegionPic from "../../assets/country-region/Pakistan.gif"
+import paRegionPic from "../../assets/country-region/Panama.gif"
+import pgRegionPic from "../../assets/country-region/Papua New Guinea.gif"
+import pyRegionPic from "../../assets/country-region/Paraguay.gif"
+import peRegionPic from "../../assets/country-region/Peru.gif"
+import phRegionPic from "../../assets/country-region/Philippines.gif"
+import plRegionPic from "../../assets/country-region/Poland.gif"
+import ptRegionPic from "../../assets/country-region/Portugal.gif"
+import qaRegionPic from "../../assets/country-region/Qatar.gif"
+import roRegionPic from "../../assets/country-region/Romania.gif"
+import ruRegionPic from "../../assets/country-region/Russia.gif"
+import rwRegionPic from "../../assets/country-region/Rwanda.gif"
+import saRegionPic from "../../assets/country-region/Saudi Arabia.gif"
+import snRegionPic from "../../assets/country-region/Senegal.gif"
+import rsRegionPic from "../../assets/country-region/Serbia.gif"
+import scRegionPic from "../../assets/country-region/Seychelles.gif"
+import slRegionPic from "../../assets/country-region/Sierra Leone.gif"
+import sgRegionPic from "../../assets/country-region/Singapore.gif"
+import skRegionPic from "../../assets/country-region/Slovakia.gif"
+import siRegionPic from "../../assets/country-region/Slovenia.gif"
+import soRegionPic from "../../assets/country-region/Somalia.gif"
+import zaRegionPic from "../../assets/country-region/South Africa.gif"
+import krRegionPic from "../../assets/country-region/South Korea.gif"
+import esRegionPic from "../../assets/country-region/Spain.gif"
+import lkRegionPic from "../../assets/country-region/Sri Lanka.gif"
+import sdRegionPic from "../../assets/country-region/Sudan.gif"
+import seRegionPic from "../../assets/country-region/Sweden.gif"
+import chRegionPic from "../../assets/country-region/Switzerland.gif"
+import syRegionPic from "../../assets/country-region/Syria.gif"
+import tjRegionPic from "../../assets/country-region/Tajikistan.gif"
+import tzRegionPic from "../../assets/country-region/Tanzania.gif"
+import thRegionPic from "../../assets/country-region/Thailand.gif"
+import tgRegionPic from "../../assets/country-region/Togo.gif"
+import tnRegionPic from "../../assets/country-region/Tunisia.gif"
+import trRegionPic from "../../assets/country-region/Turkey.gif"
+import tmRegionPic from "../../assets/country-region/Turkmenistan.gif"
+import ugRegionPic from "../../assets/country-region/Uganda.gif"
+import uaRegionPic from "../../assets/country-region/Ukraine.gif"
+import aeRegionPic from "../../assets/country-region/United Arab Emirates.gif"
+import gbRegionPic from "../../assets/country-region/United Kingdom.gif"
+import usRegionPic from "../../assets/country-region/United States.gif"
+import uyRegionPic from "../../assets/country-region/Uruguay.gif"
+import uzRegionPic from "../../assets/country-region/Uzbekistan.gif"
+import veRegionPic from "../../assets/country-region/Venezuela.gif"
+import vnRegionPic from "../../assets/country-region/Vietnam.gif"
+import yeRegionPic from "../../assets/country-region/Yemen.gif"
+import zmRegionPic from "../../assets/country-region/Zambia.gif"
+import zwRegionPic from "../../assets/country-region/Zimbabwe.gif"
+
+
+export const regionImages: Record<string, string> = {
+    af: afRegionPic,
+    al: alRegionPic,
+    dz: dzRegionPic,
+    ao: aoRegionPic,
+    ar: arRegionPic,
+    au: auRegionPic,
+    at: atRegionPic,
+    az: azRegionPic,
+    bd: bdRegionPic,
+    by: byRegionPic,
+    be: beRegionPic,
+    bj: bjRegionPic,
+    bo: boRegionPic,
+    bg: bgRegionPic,
+    bf: bfRegionPic,
+    bi: biRegionPic,
+    kh: khRegionPic,
+    cm: cmRegionPic,
+    ca: caRegionPic,
+    cf: cfRegionPic,
+    td: tdRegionPic,
+    cl: clRegionPic,
+    cn: cnRegionPic,
+    co: coRegionPic,
+    cg: cgRegionPic,
+    cr: crRegionPic,
+    hr: hrRegionPic,
+    cu: cuRegionPic,
+    cz: czRegionPic,
+    dk: dkRegionPic,
+    do: doRegionPic,
+    ec: ecRegionPic,
+    eg: egRegionPic,
+    sv: svRegionPic,
+    er: erRegionPic,
+    ee: eeRegionPic,
+    et: etRegionPic,
+    fj: fjRegionPic,
+    fi: fiRegionPic,
+    fr: frRegionPic,
+    ga: gaRegionPic,
+    gm: gmRegionPic,
+    ge: geRegionPic,
+    de: deRegionPic,
+    gh: ghRegionPic,
+    gr: grRegionPic,
+    gt: gtRegionPic,
+    ht: htRegionPic,
+    hn: hnRegionPic,
+    hu: huRegionPic,
+    is: isRegionPic,
+    in: inRegionPic,
+    id: idRegionPic,
+    ir: irRegionPic,
+    iq: iqRegionPic,
+    ie: ieRegionPic,
+    il: ilRegionPic,
+    it: itRegionPic,
+    jp: jpRegionPic,
+    jo: joRegionPic,
+    kz: kzRegionPic,
+    ke: keRegionPic,
+    kw: kwRegionPic,
+    kg: kgRegionPic,
+    la: laRegionPic,
+    lv: lvRegionPic,
+    lb: lbRegionPic,
+    lr: lrRegionPic,
+    ly: lyRegionPic,
+    lt: ltRegionPic,
+    lu: luRegionPic,
+    mg: mgRegionPic,
+    my: myRegionPic,
+    mv: mvRegionPic,
+    mt: mtRegionPic,
+    mx: mxRegionPic,
+    md: mdRegionPic,
+    mn: mnRegionPic,
+    me: meRegionPic,
+    ma: maRegionPic,
+    mz: mzRegionPic,
+    mm: mmRegionPic,
+    na: naRegionPic,
+    np: npRegionPic,
+    nl: nlRegionPic,
+    nz: nzRegionPic,
+    ni: niRegionPic,
+    ne: neRegionPic,
+    ng: ngRegionPic,
+    kp: kpRegionPic,
+    no: noRegionPic,
+    om: omRegionPic,
+    pk: pkRegionPic,
+    pa: paRegionPic,
+    pg: pgRegionPic,
+    py: pyRegionPic,
+    pe: peRegionPic,
+    ph: phRegionPic,
+    pl: plRegionPic,
+    pt: ptRegionPic,
+    qa: qaRegionPic,
+    ro: roRegionPic,
+    ru: ruRegionPic,
+    rw: rwRegionPic,
+    sa: saRegionPic,
+    sn: snRegionPic,
+    rs: rsRegionPic,
+    sc: scRegionPic,
+    sl: slRegionPic,
+    sg: sgRegionPic,
+    sk: skRegionPic,
+    si: siRegionPic,
+    so: soRegionPic,
+    za: zaRegionPic,
+    kr: krRegionPic,
+    es: esRegionPic,
+    lk: lkRegionPic,
+    sd: sdRegionPic,
+    se: seRegionPic,
+    ch: chRegionPic,
+    sy: syRegionPic,
+    tj: tjRegionPic,
+    tz: tzRegionPic,
+    th: thRegionPic,
+    tg: tgRegionPic,
+    tn: tnRegionPic,
+    tr: trRegionPic,
+    tm: tmRegionPic,
+    ug: ugRegionPic,
+    ua: uaRegionPic,
+    ae: aeRegionPic,
+    gb: gbRegionPic,
+    us: usRegionPic,
+    uy: uyRegionPic,
+    uz: uzRegionPic,
+    ve: veRegionPic,
+    vn: vnRegionPic,
+    ye: yeRegionPic,
+    zm: zmRegionPic,
+    zw: zwRegionPic
+}
